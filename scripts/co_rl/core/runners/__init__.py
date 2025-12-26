f1b8817396a7fcee7bdab443ac03d9cde50e0d28
cocelo_002 <jhjo1997@naver.com>
2025-12-26 16:54:10 +0900

    first commit

---
+++ scripts/co_rl/core/runners/__init__.py
@@ -0,0 +1,9 @@
+#  Copyright 2021 ETH Zurich, NVIDIA CORPORATION
+#  SPDX-License-Identifier: BSD-3-Clause
+
+"""Implementation of runners for environment-agent interaction."""
+
+from .on_policy_runner import OnPolicyRunner
+from .off_policy_runner import OffPolicyRunner
+
+__all__ = ["OnPolicyRunner", "OffPolicyRunner"]
