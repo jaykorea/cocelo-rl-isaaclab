81a46871b3cd37d7c726de6219ed953436a76896
Sanghyun Ryoo <sanghyunryoo@postech.ac.kr>
2026-07-03 00:24:11 +0900

    feat(humanoid): asset bug fix(yaw axis)

--- lab/cocelo/assets/flamingo/humanoid_light_v1.py
+++ lab/cocelo/assets/flamingo/humanoid_light_v1.py
@@ -64,7 +64,7 @@ def get_humanoid_light_joint_motor_type(joint_name: str) -> str:
 
 HUMANOID_LIGHT_CFG = ArticulationCfg(
     spawn=sim_utils.UsdFileCfg(
-        usd_path=f"{FLAMINGO_ASSETS_DATA_DIR}/Robots/Flamingo/humanoid_light_rev_1_0_9_45/humanoid_light_rev_1_0_9_45.usd",
+        usd_path=f"{FLAMINGO_ASSETS_DATA_DIR}/Robots/Flamingo/humanoid_light_rev_1_0_10_45/humanoid_light_rev_1_0_10_45.usd",
         activate_contact_sensors=True,
         rigid_props=sim_utils.RigidBodyPropertiesCfg(
             disable_gravity=False,
