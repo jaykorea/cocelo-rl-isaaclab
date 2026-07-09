7209ff557529005d5b8d0dde8c984f7bec42d8d5
Sanghyun Ryoo <sanghyunryoo@postech.ac.kr>
2026-07-09 10:57:31 +0900

    feat(humanoid): modified config file

--- lab/cocelo/tasks/manager_based/locomotion/velocity/actuators/gear_actuator.py
+++ lab/cocelo/tasks/manager_based/locomotion/velocity/actuators/gear_actuator.py
@@ -1,6 +1,7 @@
 from __future__ import annotations
 from isaaclab.actuators import DelayedPDActuator
 from isaaclab.utils.types import ArticulationActions
+import torch
 
 class GearDelayedPDActuator(DelayedPDActuator):
     """Delayed PD actuator with an additional gear ratio and gamma scaling on applied torques."""
@@ -14,7 +15,7 @@ class GearDelayedPDActuator(DelayedPDActuator):
         joint_vel = joint_vel * self.cfg.gear_ratio # OBS_{joint space} -> OBS_{motor space}
         # 먼저 DelayedPDActuator의 compute를 호출해서 delayed torque 계산
         control_action = super().compute(control_action, joint_pos, joint_vel) # motor space에서 torque 계산
-
+        
         # 여기서 gear ratio를 곱해줌
         if self.cfg.gear_ratio != 1.0:
             self.computed_effort = self.computed_effort * self.cfg.gear_ratio * self.cfg.gamma # TORQUE_{motor space} -> TORQUE_{joint space}
@@ -52,7 +53,7 @@ class CoupledDelayedPDActuator(DelayedPDActuator):
         For example:
             pitch_joint slot -> motor_1 target
             roll_joint slot  -> motor_2 target
-    """
+    """ 
 
     cfg: "CoupledDelayedPDActuatorCfg"
 
